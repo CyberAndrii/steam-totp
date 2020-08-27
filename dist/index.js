995fd2ab6a323d052a28ce48aa03e41bc3aacbe4
Andrii Vitiv <cyberandrii@gmail.com>
2020-08-28 02:30:23 +0300

    Fix time offset

--- dist/index.js
+++ dist/index.js
@@ -402,15 +402,23 @@ function run()
     try
     {
     	const sharedSecret = core.getInput('shared_secret', { required: true });
-    	const timeOffset = core.getInput('time_offset');
+    	const timeOffset = parseInt(core.getInput('time_offset'));
 
     	if(sharedSecret.length !== 28 || !sharedSecret.endsWith('='))
     	{
-    		throw Error('Invalid shared secret.')
+    		throw Error('Invalid shared secret.');
     	}
 
-    	const code = totp.generateAuthCode(sharedSecret, timeOffset);
-        core.setOutput('code', code);
+    	totp.getTimeOffset((error, offset, latency) => 
+    	{
+    		if(error)
+    		{
+    			throw error;
+    		}
+
+    		const code = totp.generateAuthCode(sharedSecret, offset + timeOffset);
+        	core.setOutput('code', code);
+    	});
     }
     catch (error)
     {
