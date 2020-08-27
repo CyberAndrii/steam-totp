d6ab45b851e03b4febc0a50c39c01bb90d3d652f
Andrii Vitiv <cyberandrii@gmail.com>
2020-08-27 23:38:34 +0300

    Check is shared_secret valid

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ function run()
     	const sharedSecret = core.getInput('shared_secret', { required: true });
     	const timeOffset = core.getInput('time_offset');
 
+    	if(sharedSecret.length !== 28 || !sharedSecret.endsWith('='))
+    	{
+    		throw Error('Invalid shared secret.')
+    	}
+
     	const code = totp.generateAuthCode(sharedSecret, timeOffset);
         core.setOutput('code', code);
     }
